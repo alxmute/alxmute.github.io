bc2a28e1705afc2b9d69c83f52f0a8c64f4e7def
Alexey Mutianov <mutianov.o.o@gmail.com>
2019-05-20 16:45:02 +0300

    replace mysql to H2 for viewing in github

--- src/main/java/ua/nmu/airportticketoffice/config/WebSecurityConfig.java
+++ src/main/java/ua/nmu/airportticketoffice/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ua.nmu.airportticketoffice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,8 +19,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
-    @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void setPasswordEncoder(@Lazy PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
